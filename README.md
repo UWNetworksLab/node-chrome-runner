86583c6da448af5e65b2f0dda6a07d07c76cd4b6
Lucas Dixon <ldixon@google.com>
2015-03-05 15:58:54 -0500

    suported version specifications

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ gulp dist # build the library
 
 ## Usage
 
-
 ```
 # Load with a relative path from the root of the node-chrome-runner repo...
 var chrome_runner = require('./build/dist/node-chrome-runner/chrome-runner');
@@ -20,25 +19,33 @@ var chrome_runner = require('./build/dist/node-chrome-runner/chrome-runner');
 var chrome_runner = require('node-chrome-runner');
 
 # Start chrome; returns the child process as `c1.childProcess`...
-var c1 = chrome_runner.runChrome();
+var c = chrome_runner.runChrome();
 
 # Console log the path used to start chrome...
-console.log(c1.path);
+console.log(c.path);
 
 # Run chrome with a custom path, and argument to make chrome start
 # with user directory `tmp/foo` on Mac...
-var c2 = chrome_runner.runChrome({
+var c = chrome_runner.runChrome({
+  versions: [chrome_runner.chromePaths.canary],
+  args:['--user-data-dir=tmp/foo'],
+  processOptions:{stdio: 'inherit'}
+});
+
+# Run chrome with a custom path, and argument to make chrome start
+# with user directory `tmp/foo` on Mac...
+var c = chrome_runner.runChrome({
   path: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary',
   args:['--user-data-dir=tmp/foo'],
   processOptions:{stdio: 'inherit'}
 });
 
 # or use platform found version, but set the user-data dir...
-var c3 = chrome_runner.runChrome({
+var c = chrome_runner.runChrome({
   args:['--user-data-dir=tmp/foo'],
   processOptions:{stdio: 'inherit'}
 });
 
 # Send a close signal to the started chrome.
-c3.childProcess.kill('SIGHUP');
+c.childProcess.kill('SIGHUP');
 ```
--- src/chrome-runner.ts
+++ src/chrome-runner.ts
@@ -6,7 +6,16 @@ import path = require('path');
 import childProcess = require('child_process');
 import fs = require('fs');
 
-export var chromePaths = {
+export interface PlatformPaths {
+  windowsXp      ?: string;
+  windowsVista   ?: string;
+  macSystem      ?: string;
+  macUser        ?: string;
+  linuxSystem    ?: string;
+  [other:string] : string;
+}
+
+var chromeStablePaths :PlatformPaths = {
   windowsXp:
       path.join(process.env['HOMEPATH'] || '',
         'Local Settings\\Application Data\\Google\\Chrome\\Application\\chrome.exe'),
@@ -15,34 +24,40 @@ export var chromePaths = {
         '\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'),
   macSystem: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
   macUser: path.join(process.env['HOME'] || '', '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'),
-  macCanarySystem: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary',
-  macCanaryUser: path.join(process.env['HOME'] || '', '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'),
   linuxSystem: '/usr/bin/google-chrome',
-  linuxSystemCanary: '/usr/bin/google-chrome-canary'
 }
 
-// Utility function to give list of operating system paths that may contain the
-// chrome binary.
-// See: https://code.google.com/p/selenium/wiki/ChromeDriver
-export function osChromePaths() :string[] {
-  if (/^win/.test(process.platform)) {
-    // Windows
-    return [chromePaths.windowsXp, chromePaths.windowsVista];
-  } else if (process.platform === "darwin") {
-    // Mac
-    return [chromePaths.macUser, chromePaths.macSystem];
-  } else {
-    // Some variant of linux.
-    return [chromePaths.linuxSystem];
+var chromeCanaryPaths :PlatformPaths = {
+  windowsXp:
+      path.join(process.env['HOMEPATH'] || '',
+        'Local Settings\\Application Data\\Google\\Chrome\ SxS\\Application\\chrome.exe'),
+  windowsVista:
+      path.join(process.env['USERPROFILE'] || '',
+        '\\AppData\\Local\\Google\\Chrome\ SxS\\Application\\chrome.exe'),
+  macSystem: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary',
+  macUser: path.join(process.env['HOME'] || '', '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'),
+  linuxSystem: '/usr/bin/google-chrome-canary'
+}
+
+export var chromePaths = {
+  stable: chromeStablePaths,
+  canary: chromeCanaryPaths
+};
+
+// Utility function to pick the first path for chrome that exists in the
+// filesystem (searches in order, lexographically first of version specified,
+// and then for that version for each platform path). Returns null if no path
+// if found.
+export function pickFirstPath(chromePathsForVersions:PlatformPaths[])
+    : string {
+  for(var i = 0; i < chromePathsForVersions.length; ++i) {
+    var chromePaths = chromePathsForVersions[i];
+    for (var pathName in chromePaths) {
+      var path = chromePaths[pathName];
+      if (fs.existsSync(path)) return path;
+    }
   }
-}
 
-// Utility function to pick the first path in the list that exists in the
-// filesystem.
-function pickFirstExistingPath(paths:string[]) : string {
-  for(var i = 0; i < paths.length; ++i) {
-    if (fs.existsSync(paths[i])) return paths[i];
-  };
   return null;
 }
 
@@ -51,16 +66,21 @@ export interface NodeChildProcessOptions
 
 // Run the chrome binary.
 export function runChrome(
-    config:{ path?:string;
-             args?:string[];
+    config:{ path     ?:string;
+             versions ?:PlatformPaths[];
+             args     ?:string[];
              processOptions?:NodeChildProcessOptions;
     } = {}) : { path :string;
                 childProcess :childProcess.ChildProcess } {
-  var chromePaths :string[] = config.path ? [config.path] : osChromePaths();
-  var chosenChromePath :string = pickFirstExistingPath(chromePaths);
+
+  var chromePathsForVersions :PlatformPaths[] =
+      config.path ? [{ other: config.path }]
+                  : config.versions
+                    || [chromePaths.stable, chromePaths.canary];
+  var chosenChromePath = pickFirstPath(chromePathsForVersions);
 
   if (!chosenChromePath) {
-    throw new Error('Cannot find Chrome binary in: ' + chromePaths.toString());
+    throw new Error('Cannot find Chrome binary in: ' + JSON.stringify(chromePathsForVersions));
   }
   return {
     path: chosenChromePath,
